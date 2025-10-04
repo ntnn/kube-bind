61ead5fa58fe42954515cd7b28c405eb447d4853
Nelo-T. Wallus <red.brush9525@fastmail.com>
2025-10-04 23:39:53 +0200

    fixup kcp e2e
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

---
+++ contrib/kcp/test/e2e/backend.go
@@ -0,0 +1,131 @@
+/*
+Copyright 2025 The Kube Bind Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/gorilla/securecookie"
+	kcpclientset "github.com/kcp-dev/kcp/sdk/client/clientset/versioned/cluster"
+	kcptestinghelpers "github.com/kcp-dev/kcp/sdk/testing/helpers"
+	kcptestingserver "github.com/kcp-dev/kcp/sdk/testing/server"
+	"github.com/kcp-dev/logicalcluster/v3"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	"github.com/kube-bind/kube-bind/backend"
+)
+
+// startBackend is a copy of framework.StartBackend but skips the CRDs
+// (which clashes with the APIResourceSchemas installed by kcp-init.
+func startBackend(t *testing.T, args ...string) (string, *backend.Server) {
+	signingKey := securecookie.GenerateRandomKey(32)
+	require.NotEmpty(t, signingKey, "error creating signing key")
+	encryptionKey := securecookie.GenerateRandomKey(32)
+	require.NotEmpty(t, encryptionKey, "error creating encryption key")
+
+	addr := "127.0.0.1:8080"
+
+	args = append(
+		[]string{
+			"--oidc-issuer-client-secret=ZXhhbXBsZS1hcHAtc2VjcmV0",
+			"--oidc-issuer-client-id=kube-bind",
+			"--oidc-issuer-url=http://127.0.0.1:5556/dex",
+			"--cookie-signing-key=" + base64.StdEncoding.EncodeToString(signingKey),
+			"--cookie-encryption-key=" + base64.StdEncoding.EncodeToString(encryptionKey),
+
+			"--listen-address=" + addr,
+			"--oidc-callback-url=http://127.0.0.1:8080/callback",
+		},
+		args...,
+	)
+
+	backendCmd := exec.CommandContext(t.Context(),
+		"../../../../bin/backend",
+		args...,
+	)
+	backendCmd.Stdout = newLogWriter("[backend stdout] ", t)
+	backendCmd.Stderr = newLogWriter("[backend stderr] ", t)
+	require.NoError(t, backendCmd.Start())
+	t.Cleanup(func() {
+		if backendCmd.Process != nil {
+			t.Logf("Stopping dex (PID: %d)", backendCmd.Process.Pid)
+			assert.NoError(t, backendCmd.Process.Kill())
+		}
+	})
+	return addr, nil
+}
+
+func bootstrapBackend(t *testing.T, server kcptestingserver.RunningServer) string {
+	t.Helper()
+	t.Log("Bootstrapping backend")
+
+	client, err := kcpclientset.NewForConfig(server.BaseConfig(t))
+	require.NoError(t, err)
+
+	exportUrl := ""
+	kcptestinghelpers.Eventually(t, func() (bool, string) {
+		exportES, err := client.Cluster(logicalcluster.NewPath("root").Join("kube-bind")).
+			ApisV1alpha1().
+			APIExportEndpointSlices().
+			Get(t.Context(), "kube-bind.io", metav1.GetOptions{})
+		if err != nil {
+			return false, fmt.Sprintf("Error getting APIExportEndpointSlice: %v", err)
+		}
+		if len(exportES.Status.APIExportEndpoints) == 0 {
+			return false, "APIExportEndpoints is empty"
+		}
+		exportUrl = exportES.Status.APIExportEndpoints[0].URL
+		return true, ""
+	}, wait.ForeverTestTimeout, time.Millisecond*100)
+	require.NotEmpty(t, exportUrl, "APIExportEndpointSlice URL is empty")
+
+	_, backendKubeconfig := wsConfig(t, server, logicalcluster.NewPath("root").Join("kube-bind"))
+
+	t.Log("Starting kube-bind backend for KCP")
+	addr, _ := startBackend(t,
+		"--kubeconfig="+backendKubeconfig,
+		"--multicluster-runtime-provider=kcp",
+		"--server-url="+exportUrl,
+		"--pretty-name=BigCorp.com",
+		"--namespace-prefix=kube-bind-",
+		"--schema-source=apiresourceschemas",
+		"--consumer-scope=cluster", // TODO configure to test both modes
+	)
+
+	t.Log("Wait for backend to be ready")
+	req, err := http.NewRequestWithContext(t.Context(), http.MethodGet, "http://"+addr+"/healthz", nil)
+	require.NoError(t, err)
+	kcptestinghelpers.Eventually(t, func() (bool, string) {
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return false, ""
+		}
+		defer resp.Body.Close()
+		return resp.StatusCode == http.StatusOK, ""
+	}, wait.ForeverTestTimeout, time.Millisecond*100)
+	t.Log("Backend is ready")
+
+	return addr
+}
